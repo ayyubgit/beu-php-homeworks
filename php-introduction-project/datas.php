8b1cac2eb5b612e48ad1335189e1895fa016da4b
ayyubgit <ayyubnajafzade@gmail.com>
2020-09-21 22:37:27 +0400

    php-introduction-project completed

---
+++ php-introduction-project/datas.php
@@ -0,0 +1,56 @@
+<?php
+    $features = [
+        (object)[
+            'title' => 'Feature 1',
+            'text' => 'Text 1 A pseudo-Latin text used in web design, 
+                                        layout, and printing in place of English to emphasise design elements.'
+        ],
+        (object)[
+            'title' => 'Feature 2',
+            'text' => 'Text 2 A pseudo-Latin text used in web design, 
+                                        layout, and printing in place of English to emphasise design elements.'
+        ],
+        (object)[
+            'title' => 'Feature 3',
+            'text' => 'Text 3 A pseudo-Latin text used in web design, 
+                                        layout, and printing in place of English to emphasise design elements.'
+        ],
+        (object)[
+            'title' => 'Feature 4',
+            'text' => 'Text 4 A pseudo-Latin text used in web design, 
+                                        layout, and printing in place of English to emphasise design elements.'
+        ],
+    ];
+    $subscriptionOffers = [
+        (object)[
+            'title' => 'Starter',
+            'price' => 27,
+            'descriptions' => [
+                'Lorem ipsum is common text 1',
+                'Lorem ipsum is common text 2',
+                'Lorem ipsum is common text 3',
+                'Lorem ipsum is common text 4',
+            ]
+        ],
+        (object)[
+            'title' => 'Professional',
+            'price' => 97,
+            'descriptions' => [
+                'Lorem ipsum is common text 5',
+                'Lorem ipsum is common text 6',
+                'Lorem ipsum is common text 7',
+                'Lorem ipsum is common text 8',
+            ]
+        ],
+        (object)[
+            'title' => 'Business',
+            'price' => 147,
+            'descriptions' => [
+                'Lorem ipsum is common text 9',
+                'Lorem ipsum is common text 10',
+                'Lorem ipsum is common text 11',
+                'Lorem ipsum is common text 12',
+            ]
+        ]
+    ]
+?>
